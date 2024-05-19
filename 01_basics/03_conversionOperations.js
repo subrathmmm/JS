7078466055647354713acdf4318c691717dc9a7d
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-19 07:57:18 +0000

    03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,3 +17,32 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+// operations
+
+let value = 6
+let negValue = -value
+console.log(negValue)
+
+let str1 = "sm"
+let str2 = " is great"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(4 + 3 + "3")
+console.log("2" + 4)
+console.log("3" + 3 + 4)
+
+console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
