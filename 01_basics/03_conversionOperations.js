d55b0d061e52846f706b2b9500c041567f9e454b
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-19 06:30:11 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "23abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+//if we write any number within double brackets then it    becomes string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = "1"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
