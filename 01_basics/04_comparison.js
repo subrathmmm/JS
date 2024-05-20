c0e166e74e8dbba561fbc71b2c82c6198b18931a
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-20 07:30:15 +0000

    04_comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+
+
+console.log("2" > 1)
+console.log("2" == 2)
+console.log("2" === 2)
+
+console.log(null == 0)
+// can't say about null as sometimes it is 0 and sometimes its nan
+// undefined always gives false
+
+console.log(undefined > 2)
+
+console.log("new")
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+
+
