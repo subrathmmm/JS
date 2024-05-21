162397c2f75e92c3845395ce33b3c9ac789b2c3c
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-21 04:59:28 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,3 +1,28 @@
 const name = "subrat"
 const repoCount = 40;
-console.log(name + repoCount + " value")
+// console.log(name + repoCount + " value")
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('su-br-tt');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString)
+
+const newStringOne = "    subrat   ";
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('sundar'));
+console.log(gameName.split('-'));
