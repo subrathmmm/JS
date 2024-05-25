59ba3eef8fab9fe82feff51bed0a377220679564
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-25 18:27:57 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+// var c = 300;
+let a = 20;
+const b = 9900;
+if(true){
+    let a = 200;
+    const b = 79;
+    // var c = 82;
+    console.log("Inner: ", a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
