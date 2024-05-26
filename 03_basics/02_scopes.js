e83c9827fd34267985a3fe8e329ca6ad91f09995
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-26 17:22:31 +0000

    basics

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,45 @@ if(true){
     let a = 200;
     const b = 79;
     // var c = 82;
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "subrat";
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    // two();
+}
+// one();
+
+if(true){
+    const username = "subrat";
+    if(username === "subrat"){
+        const website = "github";
+        console.log(`this site is ${website} and username is ${username}`);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(3));
+function addone(num){
+    return num+1;
+}
+
+const addtwo = function(num){
+    return num+2;
+}
+console.log(addtwo(56));
+
