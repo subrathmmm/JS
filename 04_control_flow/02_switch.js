37e68cb9ac615b6c124d6b708cd76ef5665494ee
subrathmmm <151946671+subrathmmm@users.noreply.github.com>
2024-05-27 16:56:01 +0000

    control flow

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+
+    case "feb":
+        console.log("february");
+        break; 
+      
+    case "march":
+        console.log("march");
+        break; 
+        
+    default:
+        console.log("default case matched");
+        break;    
+}
